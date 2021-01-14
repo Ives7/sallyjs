a9b850748fc0a024d02bd046694b839322191ae5
李强 <mine@ives.pro>
2021-01-14 10:33:41 +0800

    修改测试demo

--- packages/create-lib/templates/package/test/index.spec.ts
+++ packages/create-lib/templates/package/test/index.spec.ts
@@ -1,33 +1,28 @@
+// @ts-nocheck
 import { add } from "../src/index"
 
 describe("add函数测试", function () {
   it("不传参数报错", function () {
-    // @ts-ignore
     expect(add).toThrow()
   })
 
   it("传null会报错", function () {
-    // @ts-ignore
     expect(() => add(1, null)).toThrow()
   })
 
   it("传布尔类型会报错", function () {
-    // @ts-ignore
     expect(() => add(1, true)).toThrow()
   })
 
   it("传array类型会报错", function () {
-    // @ts-ignore
     expect(() => add([], true)).toThrow()
   })
 
   it("传入字符串也可以兼容", function () {
-    // @ts-ignore
     expect(add("1", "2")).toBe(3)
   })
 
   it("正常数字可以算出正确的结果", function () {
-    // @ts-ignore
     const a = Math.ceil(Math.random() * 10)
     const b = Math.ceil(Math.random() * 10)
     expect(add(a, b)).toBe(a + b)
